b96ae1233635a0b4a634112dcca1137fbcb65b70
1170753 <akashgowda200201@gmail.com>
2023-10-10 01:07:51 +0530

    done

--- src/main/java/Automation/Restaurant/DeleteDish.java
+++ src/main/java/Automation/Restaurant/DeleteDish.java
@@ -1,7 +1,41 @@
 package Automation.Restaurant;
 
+import static io.restassured.RestAssured.given;
+
+import org.testng.Assert;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
 public class DeleteDish {
-	
+	public static void main(String[] args) {
+        RequestSpecification req = new RequestSpecBuilder()
+                .setBaseUri("http://localhost:8080/restaurants")
+                .setContentType(ContentType.JSON)
+                .build();
+
+        ResponseSpecification res = new ResponseSpecBuilder()
+                .expectStatusCode(404)
+                .expectContentType("text/plain;charset=UTF-8") 
+                .build();
+
+        RequestSpecification request = given()
+                .spec(req)
+                .pathParam("dishId", 1)
+                .log().all();
+
+        Response resp = request 
+                .log().all().when().delete("/deleteDishById/{dishId}").then().spec(res).extract().response();
+
+        String responseText = resp.getBody().asString();
+        System.out.println(responseText);
+        
+        Assert.assertEquals(responseText,"Dish not found");
+    }
 	
 
 }
